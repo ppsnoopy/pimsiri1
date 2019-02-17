0a765b369a787a2fd284f737f405e853d3947358
ppsnoopy <ppsnoopy4@gmail.com>
2019-02-17 12:25:30 +0700

    pimsiriiii

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { user } from '../user';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  
   formGroup: FormGroup;
-
   constructor(
-    private formbuilder: FormBuilder
-  ) { }
+    private formBuild: FormBuilder
+  ) 
+  {
+
+   }
 
   ngOnInit() {
-    this.formGroup = this.formbuilder.group({
-      firstname: this.formbuilder.control(''),
-      lastname:['']
-    })
+    this.formGroup = this.formBuild.group(
+      {
+      firstName: this.formBuild.control('pimsiri'),
+      lastName: ['chotikamongkhol'],
+      Email:['ppsnoopy44@gmail.com'],
+      number1: ['']
+      } )
+  }
+  onSubmit(form: FormGroup){
+    const {firstName, lastName, Email , number1} = form.value;
+    const user1 = new user(firstName, lastName, Email, number1);
+    console.log(user);
   }
 
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -3,7 +3,9 @@ import { OnInit } from '@angular/core';
 export class  user {
  constructor (
       public firstname: string,
-      public lastname: string
+      public lastname: string,
+      public Email: string,
+      public number1: number
  ){
 
  }
